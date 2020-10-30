44f44ada63c8494d8e9306d163ce4c8f5d208d39
Orest Gkini <orestijuve@gmail.com>
2020-10-30 23:54:21 +0100

    Add changeVehicle()

--- src/model/Solution.java
+++ src/model/Solution.java
@@ -4,6 +4,28 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Solution {
-    HashMap<Vehicle, List<Task>> nextTask;
+
+    HashMap<Vehicle, List<Task>> nextTask; // Maps a vehicle to a list of tasks that need to be carried in order.
+    HashMap<Task, Vehicle> taskVehicles; // Maps tasks to the vehicles that carry them.
+
+    // Returns the first task of the given vehicle.
+    public Task getFirstTaskOf(Vehicle vehicle) {
+        return nextTask.get(vehicle).get(0);
+    }
+
+    // Removes the first task of the given vehicle.
+    public void removeFirstTaskOf(Vehicle vehicle) {
+        nextTask.get(vehicle).remove(0);
+    }
+
+    // Insert the given task as the first one in the list of the given vehicle.
+    public void insertFirstTaskTo(Vehicle vehicle, Task task) {
+        nextTask.get(vehicle).add(0, task);
+    }
+
+    // Updates the vehicles of the given task in the map.
+    public void updateTaskVehicle(Task task, Vehicle vehicle) {
+        taskVehicles.put(task, vehicle);
+    }
 
 }
--- src/sls/StochasticLocalSearch.java
+++ src/sls/StochasticLocalSearch.java
@@ -1,5 +1,27 @@
 package sls;
 
+import model.Vehicle;
+import model.Solution;
+import model.Task;
+
 public class StochasticLocalSearch {
+
+    // Assigns the first task of vehicle v1 to vehicle v2.
+    private Solution changeVehicle(Solution solution, Vehicle v1, Vehicle v2) {
+        // Create a copy of the given solution.
+        newSolution = new Solution(solution);
+
+        // Get the first task of vehicle v1.
+        Task task = solution.getFirstTaskOf(v1);
+
+        // Remove it from its list of tasks (in the new solution).
+        newSolution.removeFirstTaskOf(v1);
+
+        // Insert it as the first task of vehicle v2.
+        newSolution.insertFirstTaskTo(v2, task);
+        newSolution.updateTaskVehicle(task, vehicle);
+
+        return newSolution;
+    }
 
 }
