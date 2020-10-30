635b6dc4e34a5ad70b5b1e578d2f93ac6a9c1949
theofilosm <theofilosm@athenarc.gr>
2020-10-30 18:40:47 +0100

    Add cpy constructor Solution

--- src/model/Solution.java
+++ src/model/Solution.java
@@ -1,9 +1,20 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 public class Solution {
-    HashMap<Vehicle, List<Task>> nextTask;
+    HashMap<Vehicle, List<Task>> nextTask = new HashMap<>();
+
+    public Solution() {}
+
+    /** Copy constructor */
+    public Solution(Solution toCopy) {
+        for (Entry<Vehicle, List<Task>> entry: toCopy.nextTask.entrySet()) {
+            this.nextTask.put( entry.getKey(), new ArrayList<>(entry.getValue()) );
+        }
+    }
 
 }
