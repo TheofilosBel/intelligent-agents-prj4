a8dbe6cfea216252aff714938fa358ce2e17ae59
theofilosm <theofilosm@athenarc.gr>
2020-10-31 16:09:09 +0100

    Add color to car debug print

--- src/model/VarVehicle.java
+++ src/model/VarVehicle.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import logist.simulation.Vehicle;
 import logist.topology.Topology.City;
+import java.awt.Color;
 
 /**
  * This class represents the vehicle variable in the COP
@@ -14,16 +15,18 @@ public class VarVehicle {
     private Integer capacity;
     private Integer costPerKm;
     private City startCity;
+    private Color color;
 
-    public VarVehicle(Integer capacity, Integer costPerKm, City startCity) {
+    public VarVehicle(Integer capacity, Integer costPerKm, City startCity, Color  color) {
         this.capacity = capacity;
         this.costPerKm = costPerKm;
         this.startCity = startCity;
+        this.color = color;
     }
 
     @Override
     public String toString() {
-        return "V {cap= " + capacity + "}";
+        return "V {cap= " + capacity + " c= " + color + "}";
     }
 
     /**
@@ -33,7 +36,7 @@ public class VarVehicle {
     public static List<VarVehicle> toVarVehicle(List<Vehicle> vehicles) {
         List<VarVehicle> ourVehicles = new ArrayList<>();
         for (Vehicle v: vehicles) {
-            ourVehicles.add(new VarVehicle(v.capacity(), v.costPerKm(), v.getCurrentCity()));
+            ourVehicles.add(new VarVehicle(v.capacity(), v.costPerKm(), v.getCurrentCity(), v.color()));
         }
         return ourVehicles;
     }
